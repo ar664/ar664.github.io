6ccc6cdac00f03a2079381ac5d47a93844c62ecf
ar664 <ar664@njit.edu>
2016-03-31 13:10:55 -0400

    Initial commit

---
+++ navtreedata.js
@@ -0,0 +1,37 @@
+var NAVTREE =
+[
+  [ "Dumb-ways-to-memorize", "index.html", [
+    [ "Todo List", "todo.html", null ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"ai__interpret_8c.html",
+"game_8h.html#a50ac2197010a8cf188eafc647122f1a3",
+"menu_8h.html#aa17ff70e4da81270ffeab1dcb19cacb6",
+"structai__function__s.html#a889d5923f6b7628af2fe824149b5a942"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
